641fcabe04dbcd84f1c34acc3a3762381663611f
LuisMiguelSG <luis10miguel1005@gmail.com>
2024-03-04 12:52:08 -0500

    Nuevo
    Nuevo

---
+++ Examples.py
@@ -0,0 +1,14 @@
+# Example 01
+name= input("Ingrese su primer nombre ")                    # Los valores en input se leen como str
+name2= input("Ingrese su segundo nombre ")
+surname1= input("Ingrese su primer apellido ")
+surname2= input("Ingrese su segundo apellido ")
+print(f"Hola, {name} {name2} {surname1} {surname2}")
+
+name= int(input("Ingrese su primer numero "))               # Por lo tanto, para tomar valores numericos convertimos str a int/float
+name2= int(input("Ingrese su segundo numero "))
+c= name + name2
+print(c)
+
+
+# Aprender "Objetos" / recomendado
